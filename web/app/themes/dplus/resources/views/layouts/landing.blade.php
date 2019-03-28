c4138595e225a018a8d468a52adcd2da4f0fc065
veggigit <estebancajina@gmail.com>
2019-03-28 13:42:25 -0300

    banner section structure ok

---
+++ web/app/themes/dplus/resources/views/layouts/landing.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html {!! get_language_attributes() !!}>
+  @include('partials.head')
+  <body @php body_class() @endphp>
+    @php do_action('get_header') @endphp
+    @include('partials.header')
+        <main class="main">
+          @yield('content')
+        </main>
+    @php do_action('get_footer') @endphp
+    @include('partials.footer')
+    @php wp_footer() @endphp
+  </body>
+</html>
---
+++ web/app/themes/dplus/resources/views/partials/frontpage-banner.blade.php
@@ -0,0 +1,10 @@
+<div class="banner">
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col">
+                <h1 class="text-center">Somos dpmas, la productora lider del sur de Chile</h1>
+                <p class="lead text-center">Tus eventos serán recordados</p>
+            </div>
+        </div>
+    </div>
+</div>
