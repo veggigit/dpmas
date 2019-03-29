731c71b09d35efe8a613ef2cc0cac30560d3a761
veggigit <estebancajina@gmail.com>
2019-03-28 23:46:05 -0300

    structure section portfolio ok

--- web/app/themes/dplus/resources/views/front-page.blade.php
+++ web/app/themes/dplus/resources/views/front-page.blade.php
@@ -3,4 +3,5 @@
 @section('content')
 @include('partials.frontpage-banner')
 @include('partials.frontpage-services')
+@include('partials.frontpage-portfolio')
 @endsection
---
+++ web/app/themes/dplus/resources/views/partials/frontpage-portfolio.blade.php
@@ -0,0 +1,40 @@
+<div class="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2 class="text-center">Últimos proyectos</h1>
+                    <p class="lead text-center">Bajada killer para proyectos</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="card-columns">
+            <!-- item -->
+            <div class="card">
+                <img class="card-img" src="https://picsum.photos/500/300" alt="Card image">
+            </div>
+            <!-- item -->
+            <div class="card">
+                <img class="card-img" src="https://picsum.photos/250/400" alt="Card image">
+            </div>
+            <!-- item -->
+            <div class="card">
+                <img class="card-img" src="https://picsum.photos/200/300" alt="Card image">
+            </div>
+            <!-- item -->
+            <div class="card">
+                <img class="card-img" src="https://picsum.photos/400/250" alt="Card image">
+            </div>
+            <!-- item -->
+            <div class="card">
+                <img class="card-img" src="https://picsum.photos/400/470" alt="Card image">
+            </div>
+            <!-- item -->
+            <div class="card">
+                <img class="card-img" src="https://picsum.photos/500/250" alt="Card image">
+            </div>
+        </div>
+    </div>
+    <!-- NOTE agregar boton load more-->
+</div>
