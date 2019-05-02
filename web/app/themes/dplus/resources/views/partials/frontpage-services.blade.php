fa1e4aeb5ddb2d73549a3548d315bf566dc44a8e
veggigit <estebancajina@gmail.com>
2019-05-02 08:45:51 -0400

    Update dependencies, add services to services partial

--- web/app/themes/dplus/resources/views/partials/frontpage-services.blade.php
+++ web/app/themes/dplus/resources/views/partials/frontpage-services.blade.php
@@ -3,14 +3,14 @@
         <div class="row">
             <div class="col">
                 <h2 class="text-center">Servicios</h1>
-                <p class="lead text-center">Bajada killer para servicios</p>
+                    <p class="lead text-center">Bajada killer para servicios</p>
             </div>
             <div class="w-100"></div>
             <!-- item -->
             <div class="col-12 col-md-4">
                 <div class="wrap-service text-center">
                     <i class="zmdi zmdi-rss"></i>
-                    <h4>Nombre del servicio</h4>
+                    <h4>Producción de eventos</h4>
                     <p>Descripcion del servicio</p>
                 </div>
             </div>
@@ -18,7 +18,7 @@
             <div class="col-12 col-md-4">
                 <div class="wrap-service text-center">
                     <i class="zmdi zmdi-rss"></i>
-                    <h4>Nombre del servicio</h4>
+                    <h4>Gestion de medios</h4>
                     <p>Descripcion del servicio</p>
                 </div>
             </div>
@@ -26,7 +26,71 @@
             <div class="col-12 col-md-4">
                 <div class="wrap-service text-center">
                     <i class="zmdi zmdi-rss"></i>
-                    <h4>Nombre del servicio</h4>
+                    <h4>Servicios audiovisuales</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Gestión comunicacional</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Gestión de Servicios</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Ferias, seminarios, congresos</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Ferias, seminarios, congresos</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Promociones</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Btl</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Diseño y creatividad</h4>
+                    <p>Descripcion del servicio</p>
+                </div>
+            </div>
+            <!-- item -->
+            <div class="col-12 col-md-4">
+                <div class="wrap-service text-center">
+                    <i class="zmdi zmdi-rss"></i>
+                    <h4>Regalos corporativos</h4>
                     <p>Descripcion del servicio</p>
                 </div>
             </div>
